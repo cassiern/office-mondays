faee8b635260fd142352bdf03f5bab21b2a52b67
Balthazar Ely <balthazarely@Balthazars-MacBook-Pro.local>
2019-07-26 10:53:24 -0600

    added other stuff to the server file

--- controllers/posts.js
+++
@@ -1,1 +0,0 @@
-console.log("i hope i one day have my own branch")
--- server.js
+++ server.js
@@ -1,19 +1,29 @@
 const express = require('express');
+const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
 const app = express();
 
-require('./db.db');
+//require the database
+require('./db/db');
 
+//require controllers
 const loginController = require('./controllers/login');
 const usersController = require('./controllers/login');
 const postController = require('./controllers/login');
 
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(methodOverride('_method'));
+
+
+//use the controllers
 app.use('/login', loginController);
 app.use('/users', usersController);
 app.use('/post', postController);
 
+//still need to link a css file to the 
 
 
-
+//LocalHost
 app.listen(3000, () => {
     console.log("the server is listening for requests...")
 });
